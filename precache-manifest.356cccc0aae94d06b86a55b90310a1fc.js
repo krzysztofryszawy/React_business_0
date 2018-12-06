b7f6f7ff05244c870206ed43d1772c8822aa7379
Krzysztof Ryszawy <krzysztof.ryszawy@gmail.com>
2018-12-06 22:09:28 +0100

    Updates

---
+++ precache-manifest.356cccc0aae94d06b86a55b90310a1fc.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "7c36bcf7eed7e0c97044b1d2fd80a148",
+    "url": "/React_business_0/static/media/item2.7c36bcf7.jpg"
+  },
+  {
+    "revision": "96ad13df43127eeb27f8",
+    "url": "/React_business_0/static/css/main.7c8f495d.chunk.css"
+  },
+  {
+    "revision": "fd3ed46fca0cbf569d95",
+    "url": "/React_business_0/static/js/1.fd3ed46f.chunk.js"
+  },
+  {
+    "revision": "2eb819f6433d9d34ef24",
+    "url": "/React_business_0/static/js/runtime~main.2eb819f6.js"
+  },
+  {
+    "revision": "cb1ee1c81cca1639ec7abb28cfa2773d",
+    "url": "/React_business_0/static/media/photo-1521590832167-7bcbfaa6381f.cb1ee1c8.jpg"
+  },
+  {
+    "revision": "218b82c00d267ed0465d3004914802ec",
+    "url": "/React_business_0/static/media/photo-1515172371186-85d50c9f1fc1.218b82c0.jpg"
+  },
+  {
+    "revision": "2515efdabc2985a65d8e0dc08232c6b6",
+    "url": "/React_business_0/static/media/item1.2515efda.jpg"
+  },
+  {
+    "revision": "96ad13df43127eeb27f8",
+    "url": "/React_business_0/static/js/main.96ad13df.chunk.js"
+  },
+  {
+    "revision": "73382eaa4f6dc1b0c285670bfa28e7aa",
+    "url": "/React_business_0/static/media/item3.73382eaa.jpg"
+  },
+  {
+    "revision": "ae570a0d3a23cf8de221019286eaea4d",
+    "url": "/React_business_0/static/media/item4.ae570a0d.jpg"
+  },
+  {
+    "revision": "ffc83c177b435870157e3643d1f58103",
+    "url": "/React_business_0/static/media/item5.ffc83c17.jpg"
+  },
+  {
+    "revision": "31cea9dfb3fe20427f969d9a8214dcd4",
+    "url": "/React_business_0/static/media/item6.31cea9df.jpg"
+  },
+  {
+    "revision": "9a7240c8b3784060b09b1468ec38568c",
+    "url": "/React_business_0/static/media/item7.9a7240c8.jpg"
+  },
+  {
+    "revision": "f38d95a37842a698656c5d0c2cc6f1e6",
+    "url": "/React_business_0/static/media/item8.f38d95a3.jpg"
+  },
+  {
+    "revision": "39564b282710b3b37eee75b266030f16",
+    "url": "/React_business_0/index.html"
+  }
+];
